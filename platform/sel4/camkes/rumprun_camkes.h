949bfe113796dc59249738ec13e792646e7f5767
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-06-27 11:14:38 +1000

    sel4: add CAmkES files

---
+++ platform/sel4/camkes/rumprun_camkes.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#define _VAR_STRINGIZE(...) #__VA_ARGS__
+#define VAR_STRINGIZE(...) _VAR_STRINGIZE(__VA_ARGS__)
+
+
+#define RUMPRUN_META_CONNECTION(instance,platform) \
+    connection seL4TimeServer instance##_timer(from instance.platform_timer, to platform.timer); \
+    connection seL4RPCCall instance##_serial(from instance.platform_putchar, to platform.putchar);
+
+
+#define RUMPRUN_COMPONENT_DEFINITION() \
+    control; \
+    uses Timer platform_timer; \
+    uses PutChar platform_putchar; \
+    attribute int simple_untyped24_pool = 5; \
+    attribute int cnode_size_bits = 16; \
+    attribute int simple = 1; \
+    attribute int rump_priority = 100; \
+    attribute RumprunConfig rump_config;
+
+#define RUMPRUN_COMPONENT_CONFIGURATION(instance, id) \
+        instance.platform_timer_global_endpoint = VAR_STRINGIZE(instance); \
+        instance.platform_timer_attributes = id + 2; \
+        instance.platform_putchar_attributes = id;
+
